ea3aa2152e7ca96c275351a514f3b1bd36f55cc0
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-30 10:59:30 +0700

    menambahkan form add wish

--- app/src/main/java/actualsolusi/com/samplesqlite/AddWishActivity.java
+++ app/src/main/java/actualsolusi/com/samplesqlite/AddWishActivity.java
@@ -2,14 +2,39 @@ package actualsolusi.com.samplesqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import data.DatabaseHandler;
+import models.MyWish;
 
 public class AddWishActivity extends AppCompatActivity {
+    private EditText etTitle,etContent;
+    private Button btnAddWish;
+    private DatabaseHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_wish);
 
+
+        etTitle = (EditText)findViewById(R.id.etTitle);
+        etContent = (EditText)findViewById(R.id.etContent);
+        btnAddWish = (Button)findViewById(R.id.btnAddWish);
+
+        db = new DatabaseHandler(AddWishActivity.this);
+        btnAddWish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyWish wish = new MyWish();
+                wish.setTitle(etTitle.getText().toString());
+                wish.setContent(etContent.getText().toString());
+                db.addWish(wish);
+                db.close();
+            }
+        });
 
     }
 }

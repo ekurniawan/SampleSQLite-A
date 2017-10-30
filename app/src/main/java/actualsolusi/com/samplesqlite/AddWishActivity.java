c99fb792e4508d9d9d2a563aa0e3b16361c66051
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-30 10:47:21 +0700

    menambahkan layout tambah wish

---
+++ app/src/main/java/actualsolusi/com/samplesqlite/AddWishActivity.java
@@ -0,0 +1,15 @@
+package actualsolusi.com.samplesqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddWishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_wish);
+
+
+    }
+}

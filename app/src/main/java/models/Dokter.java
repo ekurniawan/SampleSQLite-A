e658e50a0cdc1b55789fc3ffca49fb918dbf0a47
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-20 09:03:59 +0700

    menambahkan model Dokter

---
+++ app/src/main/java/models/Dokter.java
@@ -0,0 +1,44 @@
+package models;
+
+/**
+ * Created by erick on 20/11/2017.
+ */
+
+public class Dokter {
+    private String Nik;
+    private String Nama;
+    private String Spesialisasi;
+    private String Alamat;
+
+    public String getNik() {
+        return Nik;
+    }
+
+    public void setNik(String nik) {
+        Nik = nik;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getSpesialisasi() {
+        return Spesialisasi;
+    }
+
+    public void setSpesialisasi(String spesialisasi) {
+        Spesialisasi = spesialisasi;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        Alamat = alamat;
+    }
+}

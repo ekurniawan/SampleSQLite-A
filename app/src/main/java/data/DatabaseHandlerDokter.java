b1f5c8c4e74a2aff2d186e568f526bfed142e505
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-20 09:13:44 +0700

    menambahkan insert update dokter

--- app/src/main/java/data/DatabaseHandlerDokter.java
+++ app/src/main/java/data/DatabaseHandlerDokter.java
@@ -1,8 +1,11 @@
 package data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import models.Dokter;
 
 /**
  * Created by erick on 20/11/2017.
@@ -16,11 +19,42 @@ public class DatabaseHandlerDokter extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String strCreateSql = "create table dokter (Nik string primary key,"+
+                "Nama text,Alamat text, Spesialisasi text);";
+        sqLiteDatabase.execSQL(strCreateSql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists dokter");
+        onCreate(sqLiteDatabase);
+    }
 
+    public void addWish(Dokter dokter){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("Nik",dokter.getNik());
+        values.put("Nama",dokter.getNama());
+        values.put("Alamat",dokter.getAlamat());
+        values.put("Spesialisasi",dokter.getSpesialisasi());
+
+        db.insert("dokter",null,values);
+        db.close();
+    }
+
+    public void deleteWish(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete("dokter","Nik=?",new String[]{id});
+        db.close();
+    }
+
+    public void updateWish(Dokter dokter){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("Nama",dokter.getNama());
+        values.put("Alamat",dokter.getAlamat());
+        values.put("Spesialisasi",dokter.getSpesialisasi());
+        db.update("dokter",values,"Nik=?",new String[]{dokter.getNik()});
+        db.close();
     }
 }
